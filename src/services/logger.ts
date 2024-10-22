9bc6aa0d76fd763313534e1fd933ae2ad390b1af
anton.skoromniy <79968255+antonskr@users.noreply.github.com>
2024-10-23 01:41:03 +0300

    added colorized logs

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -3,8 +3,11 @@ import { createLogger, format, transports } from 'winston';
 const logger = createLogger({
     level: 'info',
     format: format.combine(
+        format.colorize(),
         format.timestamp(),
-        format.json()
+        format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} ${level}: ${message}`;
+          })
     ),
     transports: [
         new transports.Console(),
